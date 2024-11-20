396ddcc95572be48287f7412b05118cfbeb49a55
sirferr <ursusmartimus007@gmail.com>
2024-11-20 10:15:46 +0300

    complete

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,62 @@
 import 'package:flutter/material.dart';
 import 'package:dio/dio.dart';
-import 'package:cached_network_image/cached_network_image.dart';
 import 'package:get_it/get_it.dart';
+
+import 'api.dart';
 
 void main() {
   setupLocator();
   runApp(const MyApp());
 }
 
-final GetIt locator = GetIt.instance;
+// Настройка GetIt
+final getIt = GetIt.instance;
 
 void setupLocator() {
-  locator.registerLazySingleton(() => DioClient());
-  locator.registerLazySingleton(() => RecipeRepository(locator<DioClient>()));
+  getIt.registerSingleton<Dio>(Dio());
+  getIt.registerLazySingleton(() => RecipeService(getIt<Dio>()));
 }
 
+// RecipeService для работы с API
+class RecipeService {
+  final Dio dio;
+  final String _apiUrl = 'https://api.api-ninjas.com/v1/recipe';
+  final String _apiKey = API;
+
+  RecipeService(this.dio);
+
+  Future<List<dynamic>> fetchRecipes(String query) async {
+    final response = await dio.get(
+      _apiUrl,
+      queryParameters: {'query': query},
+      options: Options(headers: {'X-Api-Key': _apiKey}),
+    );
+    return response.data;
+  }
+}
+
+// InheritedWidget для управления состоянием
+class RecipeInheritedWidget extends InheritedWidget {
+  final List<dynamic> recipes;
+  final bool isLoading;
+
+  const RecipeInheritedWidget({
+    required this.recipes,
+    required this.isLoading,
+    required Widget child,
+  }) : super(child: child);
+
+  static RecipeInheritedWidget? of(BuildContext context) {
+    return context.dependOnInheritedWidgetOfExactType<RecipeInheritedWidget>();
+  }
+
+  @override
+  bool updateShouldNotify(covariant RecipeInheritedWidget oldWidget) {
+    return recipes != oldWidget.recipes || isLoading != oldWidget.isLoading;
+  }
+}
+
+// Основное приложение
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
@@ -22,51 +64,61 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Рецепты',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const RecipeListScreen(),
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: const RecipeProvider(),
     );
   }
 }
 
-class RecipeListScreen extends StatefulWidget {
-  const RecipeListScreen({Key? key}) : super(key: key);
+// Провайдер, связывающий GetIt и InheritedWidget
+class RecipeProvider extends StatefulWidget {
+  const RecipeProvider({Key? key}) : super(key: key);
 
   @override
-  _RecipeListScreenState createState() => _RecipeListScreenState();
+  State<RecipeProvider> createState() => _RecipeProviderState();
 }
 
-class _RecipeListScreenState extends State<RecipeListScreen> {
-  final RecipeRepository _recipeRepository = locator<RecipeRepository>();
+class _RecipeProviderState extends State<RecipeProvider> {
+  final RecipeService _recipeService = getIt<RecipeService>();
 
   List<dynamic> _recipes = [];
   bool _isLoading = false;
 
   Future<void> fetchRecipes(String query) async {
-    setState(() {
-      _isLoading = true;
-    });
+    setState(() => _isLoading = true);
     try {
-      final response = await _recipeRepository.fetchRecipes(query);
-      setState(() {
-        _recipes = response;
-      });
+      _recipes = await _recipeService.fetchRecipes(query);
     } catch (e) {
-      print('Ошибка при получении рецептов: $e');
+      print('Ошибка: $e');
     } finally {
-      setState(() {
-        _isLoading = false;
-      });
+      setState(() => _isLoading = false);
     }
   }
 
   @override
   Widget build(BuildContext context) {
+    return RecipeInheritedWidget(
+      recipes: _recipes,
+      isLoading: _isLoading,
+      child: RecipeListScreen(fetchRecipes: fetchRecipes),
+    );
+  }
+}
+
+// Экран с отображением рецептов
+class RecipeListScreen extends StatelessWidget {
+  final Function(String) fetchRecipes;
+
+  const RecipeListScreen({Key? key, required this.fetchRecipes})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final recipes = RecipeInheritedWidget.of(context)!.recipes;
+    final isLoading = RecipeInheritedWidget.of(context)!.isLoading;
+
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Рецепты'),
-      ),
+      appBar: AppBar(title: const Text('Рецепты')),
       body: Column(
         children: [
           Padding(
@@ -76,85 +128,25 @@ class _RecipeListScreenState extends State<RecipeListScreen> {
                 labelText: 'Поиск рецептов',
                 border: OutlineInputBorder(),
               ),
-              onSubmitted: (query) {
-                fetchRecipes(query);
-              },
+              onSubmitted: fetchRecipes,
             ),
           ),
-          Expanded(
-            child: _isLoading
-                ? const Center(child: CircularProgressIndicator())
-                : ListView.builder(
-              itemCount: _recipes.length,
+          isLoading
+              ? const Center(child: CircularProgressIndicator())
+              : Expanded(
+            child: ListView.builder(
+              itemCount: recipes.length,
               itemBuilder: (context, index) {
-                final recipe = _recipes[index];
-                return Card(
-                  margin: const EdgeInsets.all(16.0),
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        ClipRRect(
-                          borderRadius: BorderRadius.circular(16.0),
-                          child: CachedNetworkImage(
-                            fit: BoxFit.contain,
-                            imageUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSMtt3aOrfYZ1KnQq4GK0vf9gkNBC07f72UWQ&s',
-                            placeholder: (context, url) => const CircularProgressIndicator(),
-                            errorWidget: (context, url, error) => const Icon(Icons.error),
-                          ),
-                        ),
-                        const SizedBox(height: 8),
-                        ListTile(
-                          title: Text(
-                            recipe['title'] ?? 'Без названия',
-                            textAlign: TextAlign.start  ,
-                          ),
-                          subtitle: Text(
-                            recipe['ingredients']?.replaceAll('|', '\n') ?? 'Нет ингредиентов',
-                            textAlign: TextAlign.start,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
+                final recipe = recipes[index];
+                return ListTile(
+                  title: Text(recipe['title'] ?? 'Без названия'),
+                  subtitle: Text(recipe['ingredients']?.replaceAll('|', '\n') ?? ''),
                 );
               },
             ),
           ),
         ],
       ),
-    );
-  }
-}
-
-
-class DioClient {
-  final Dio _dio = Dio();
-
-  Future<List<dynamic>> get(String url, {Map<String, dynamic>? queryParameters, Map<String, String>? headers}) async {
-    final response = await _dio.get(
-      url,
-      queryParameters: queryParameters,
-      options: Options(headers: headers),
-    );
-    return response.data;
-  }
-}
-
-class RecipeRepository {
-  final DioClient _dioClient;
-  final String _apiUrl = 'https://api.api-ninjas.com/v1/recipe';
-  final String _apiKey = '';
-
-  RecipeRepository(this._dioClient);
-
-  Future<List<dynamic>> fetchRecipes(String query) async {
-    return await _dioClient.get(
-      _apiUrl,
-      queryParameters: {'query': query},
-      headers: {'X-Api-Key': _apiKey},
     );
   }
 }
